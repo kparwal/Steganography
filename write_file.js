b893ed2d7d33513ffe2d2dceac8b753767db0391
Jason Libbey <jelgt2011@gmail.com>
2015-09-26 16:26:23 -0400

    move encoding

--- write_file.js
+++ write_file.js
@@ -1,30 +1,44 @@
-var fs = require('fs');
 
-// fs.writeFile(".\\test.txt", "THIS IS A TEST", function (err) {
-//     if (err) {
-//       console.log(err)
-//     }
-//     console.log("Success!")
-//   }
-// );
 
-// fs.readFile('.\\Untitled.bmp', {encoding: "utf8"}, function(err, data) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(data.toString('hex'));
-// });
+function handleFiles(e) {
+  var file = e.target.files[0];
+  var reader = new FileReader();
+  reader.addEventListener("load", processimage, false);
+  reader.readAsArrayBuffer(file);
+}
 
-// var file = require("fs").createReadStream("Untitled.bmp", { encoding: "utf8" });
-// file.pipe(process.stdout);
+function processimage(e) {
+  var buffer = e.target.result;
+  var bitmap = getBMP(buffer);
+}
 
-fs.open('Untitled.bmp', 'r', function(status, fd) {
-    if (status) {
-        console.log(status.message);
-        return;
-    }
-    var buffer = new Buffer(100);
-    fs.read(fd, buffer, 0, 100, 0, function(err, num) {
-        console.log(buffer.toString('utf-8', 45, num));
-    });
-});
+function getBMP(buffer) {
+  var datav = new DataView(buffer);
+  var bitmap = {};
+
+  bitmap.fileheader = {};
+  bitmap.fileheader.bfType = datav.getUint16(0, true);
+  bitmap.fileheader.bfSize = datav.getUint32(2, true);
+  bitmap.fileheader.bfReserved1 = datav.getUint16(6, true);
+  bitmap.fileheader.bfReserved2 = datav.getUint16(8, true);
+  bitmap.fileheader.bfOffBits = datav.getUint32(10, true);
+
+  bitmap.infoheader = {};
+  bitmap.infoheader.biSize = datav.getUint32(14, true);
+  bitmap.infoheader.biWidth = datav.getUint32(18, true);
+  bitmap.infoheader.biHeight = datav.getUint32(22, true);
+  bitmap.infoheader.biPlanes = datav.getUint16(26, true);
+  bitmap.infoheader.biBitCount = datav.getUint16(28, true);
+  bitmap.infoheader.biCompression = datav.getUint32(30, true);
+  bitmap.infoheader.biSizeImage = datav.getUint32(34, true);
+  bitmap.infoheader.biXPelsPerMeter = datav.getUint32(38, true);
+  bitmap.infoheader.biYPelsPerMeter = datav.getUint32(42, true);
+  bitmap.infoheader.biClrUsed = datav.getUint32(46, true);
+  bitmap.infoheader.biClrImportant = datav.getUint32(50, true);
+
+  var start = bitmap.fileheader.bfOffBits;
+  bitmap.stride = Math.floor((bitmap.infoheader.biBitCount * bitmap.infoheader.biWidth + 31) / 32) * 4;
+  bitmap.pixels = new Uint8Array(buffer, start);
+  debugger;
+  return bitmap;
+}
